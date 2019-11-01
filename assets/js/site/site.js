51d40d3551aa936bb3ce53c57f2be4309403343b
Robert Crooks <rcrooks@brightcove.com>
2019-11-01 14:51:09 -0400

    wip

--- assets/js/site/site.js
+++ assets/js/site/site.js
@@ -1,26 +1,30 @@
-var BCLS_site = ( function (window, document) {
-  var all_sidenav_links = document.querySelectorAll('.sidenav a'),
+var BCLS_site = (function(window, document) {
+  var all_sidenav_links = document.querySelectorAll(".sidenav a"),
     href = window.location.pathname,
     i,
     iMax,
+    currentLink,
     p1,
     p2,
     pSib,
     pChild;
-      
+
   iMax = all_sidenav_links.length;
   for (i = 0; i < iMax; i++) {
-    if (all_sidenav_links[i].getAttribute('href') === href) {
-      all_sidenav_links[i].setAttribute('class', 'bcls-active');
-      all_sidenav_links[i].parentElement.setAttribute('class', 'bcls-active');
-      p1 = all_sidenav_links[i].parentNode;
-      pSib = p1.nextSibling;
-      pChild = p1.firstChild;
-      if (p1.nodeName === 'LI') {
-        p2 = p1.parentNode;
-        p2.setAttribute('style', 'display:block');
-      } else if (pSib.nodeName === 'H5') {
-
+    currentLink = all_sidenav_links[i];
+    if (currentLink.getAttribute("href") === href) {
+      currentLink.setAttribute("class", "bcls-active");
+      currentLink.parentElement.setAttribute("class", "bcls-active");
+      if (currentLink.getAttribute("href") !== "/") {
+        p1 = currentLink.parentElement;
+        pSib = p1.previousElementSibling;
+        pNextSib = pSib.previousElementSibling;
+        if (p1.nodeName === "LI") {
+          p2 = p1.parentNode;
+          p2.setAttribute("style", "display:block");
+        } else if (pSib.nodeName === "H5") {
+          pNextSib.setAttribute("style", "display:block");
+        }
       }
     }
   }

4f906cec1515be575c4ae82bac88b02187941918
Matthew Boles <mboles@brightcove.com>
2022-04-25 13:22:58 -0400

    $COMMITMESSAGE

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,16 +1,9 @@
 cd _includes
-git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
-<<<<<<< HEAD
-cd ..
-cd assets/images
-git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
-cd ..
-git submodule update --init --recursive
-=======
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
+git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
 cd ..
->>>>>>> 0ded3e861bfe3b58e5b1fee8e10fc182aabbce5f
+git submodule update --init --recursive

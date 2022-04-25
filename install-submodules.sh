c1aea84eecde9465eb398215be9f47a14414f102
Robert Crooks <rcrooks@brightcove.com>
2022-04-25 11:41:28 -0400

    add install-submodules.sh

---
+++ install-submodules.sh
@@ -0,0 +1,8 @@
+cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
+cd ..
+cd assets/images
+git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
+cd ..

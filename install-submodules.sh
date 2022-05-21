e5056698203924c5ccd2e7d1904aaad112e12706
Robert Crooks <rcrooks@brightcove.com>
2022-05-21 14:36:22 -0400

    adding includes and support submodules

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,14 +1,18 @@
 git submodule add https://github.com/BrightcoveLearning/s-scripts
 cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps
 git submodule add https://github.com/BrightcoveLearning/s-general
+git submodule add https://github.com/BrightcoveLearning/s-support
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps-img
 git submodule add https://github.com/BrightcoveLearning/s-general-img
+git submodule add https://github.com/BrightcoveLearning/s-support-img
+git submodule add https://github.com/BrightcoveLearning/s-feather-img
 cd ..
 git submodule update --init --recursive

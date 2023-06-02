ae73f13996a86290ba737b878e75d93bb87419d3
Jeff Doktor <doktorj7@users.noreply.github.com>
2023-06-02 09:23:38 -0600

    add analytics submodule

--- install-submodules.sh
+++ install-submodules.sh
@@ -12,6 +12,7 @@ git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-training
 git submodule add https://github.com/BrightcoveLearning/s-monetization
 git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
+git submodule add https://github.com/BrightcoveLearning/s-analytics 
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
@@ -22,6 +23,7 @@ git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
 git submodule add https://github.com/BrightcoveLearning/s-training-img
 git submodule add https://github.com/BrightcoveLearning/s-monetization-img
+git submodule add https://github.com/BrightcoveLearning/s-analytics-img 
 cd ..
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
